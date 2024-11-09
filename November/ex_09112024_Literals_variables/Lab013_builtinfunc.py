bd619548287d5e9bed7d1274a19b3b086dd392fc
soundarya <ummanenisoundarya@gmail.com>
2024-11-10 00:16:18 +0530

    10th nov

--- November/ex_09112024_Literals_variables/Lab013_builtinfunc.py
+++ November/ex_09112024_Literals_variables/Lab013_builtinfunc.py
@@ -1,11 +1,37 @@
 # predefined functions or built in functions
 a = max(12, 0) #max()
-b = min(-1, 0)#min()
-c=pow(2,3)#pow()
-d=abs(-100)#abs()
-e=input("enter ur name pls:")#input()
 print(a)
+b = min(-1, 0)#min()
 print(b)
+c=pow(2,3)#pow()
 print(c)
+d=abs(-100)#abs()
 print(d)
+e=input("enter ur name pls:")#input()
 print(e)
+f="101"#int()
+print(type(f))
+f=int(f)
+print(f)
+print(type(f))
+
+g=101 #float()
+print(type(f))
+g=float(g)
+print(g)
+print(type(g))
+
+h=101 # str()
+print(type(h))
+h=str(h)
+print(h)
+print(type(h))
+
+
+
+
+
+
+
+
+
