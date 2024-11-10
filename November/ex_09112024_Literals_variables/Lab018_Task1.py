62def211eda5a3a2b1ad66f99cda9521eb98d4f3
soundarya <ummanenisoundarya@gmail.com>
2024-11-10 21:18:36 +0530

    10th nov

---
+++ November/ex_09112024_Literals_variables/Lab018_Task1.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number:"))
+b=int(input("enter Second number:"))
+c=int(input("enter Third number:"))
+Sum=a+b+c
+sub=a-b-c
+div=a/b
+mul=a*b*c
+print("addition of the 3 numbers is:",Sum)
+print("subtraction of the 3 numbers is:",sub)
+print("division of the 2 numbers is:",div)
+print("multiplication of the 3 numbers is:",mul)
