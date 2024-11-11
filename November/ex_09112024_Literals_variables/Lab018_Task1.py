3d61c6be95dbdfba5b95cbb9a14a30f3b038bffc
soundarya <ummanenisoundarya@gmail.com>
2024-11-12 00:22:29 +0530

    9th nov

--- November/ex_09112024_Literals_variables/Lab018_Task1.py
+++ November/ex_09112024_Literals_variables/Lab018_Task1.py
@@ -3,9 +3,9 @@ b=int(input("enter Second number:"))
 c=int(input("enter Third number:"))
 Sum=a+b+c
 sub=a-b-c
-div=a/b
+div=(a/b)/c
 mul=a*b*c
 print("addition of the 3 numbers is:",Sum)
 print("subtraction of the 3 numbers is:",sub)
-print("division of the 2 numbers is:",div)
+print("division of the 312 numbers is:",div)
 print("multiplication of the 3 numbers is:",mul)
